282adafe41c8df3988b5345fe16fb12612910181
nicolas <nicolaspinto439@gmail.com>
2024-11-14 21:06:52 -0300

    Ejercicios de JS

---
+++ 07.js
@@ -0,0 +1,12 @@
+function esTipoDato(valor) {
+  // La función recibe un argumento llamado valor.
+  // Retorna el tipo de dato de este valor.
+  // Por ejemplo: "string", "number", "boolean", "object", etc.
+  // Tu código:
+  let tipodato = typeof valor
+  return tipodato;
+  }
+
+
+
+module.exports = esTipoDato;
---
+++ 18.js
@@ -0,0 +1,27 @@
+function esVocal(letra) {
+  // La función recibe un argumento llamado letra el cual es un string.
+  // Retorna como resultado un string que indica si el argumento letra es una vocal.
+  // de lo contrario retorna el string "Dato incorrecto"
+  // Por ejemplo: 
+  // "a" ---> "Es vocal"
+  // "u" ---> "Es vocal"
+  // "n" ---> "Dato incorrecto"
+  // "texto largo" ---> "Dato incorrecto"
+  // Tu código:
+  if(letra === "a"){
+    return "Es vocal"
+  } else if(letra === "e"){
+    return "Es vocal"
+  } else if(letra === "i"){
+    return "Es vocal"
+  } else if(letra === "o"){
+    return "Es vocal"
+  } else if(letra === "u"){
+    return "Es vocal"
+  } else{
+    return "Dato incorrecto"
+  
+  }
+}
+
+module.exports = esVocal;
